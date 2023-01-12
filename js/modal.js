508032554fa59953dfb6ad77b75b9db79de30792
Ignacio Cordiglia <ignaciocordiglia.contactos@gmail.com>
2023-01-11 21:44:38 -0300

    modal responsive

--- js/modal.js
+++ js/modal.js
@@ -4,25 +4,32 @@ modal1.innerHTML +=
 `
 <section class="modal m1" >
 <div class="modal__container">
-<div class="col-lg-4">
-    <img src="assets/img/productos/productos/maceta yerba.webp" class=" img-modal">
-    <p>* Venta al por mayor a partir de 20 piezas</p>
+<div class="col-lg-4 pl-lg-5 p-lg-4 justify-content-center">
+    <img src="assets/img/productos/productos/maceta yerba.webp" class="d-none d-lg-flex col-12 img-modal">
+    <p class="d-none d-lg-flex">* Venta al por mayor a partir de 20 piezas</p>
 </div>
-<div class="col-lg-5">
-<h2 class="f-reg">Maceta de Yerba</h2>
-<p>Macetas de yerba reciclada, ecologias y biodegradables. Ideales para utilizar tanto en el interior com en el exterior</p>
-<div class="btn-category">
-    <button class="btn btn-light">Chico</button>
-    <button class="btn btn-light">Mediano</button>
-    <button class="btn btn-light">Grande</button>
-    <button class="btn btn-light">Extragrande</button>
+<div class="p-3 col-12 col-lg-8 d-flex flex-wrap justify-content-center align-items-center">
+<div class="d-flex flex-row col-12">
+    <h2 class="f-reg align-self-center col-10 col-sm-11 p-sm-3 p-lg-0">Maceta de Yerba</h2>
+    <a href="#" class="modal__close justify-self-end col-2 col-sm-1"><b class="align-self-center">Cerrar</b></a>
 </div>
-<div class="text-center"> 
-    <div class="may-men">
+<p class="p-sm-3 p-lg-0" align="justify">Macetas de yerba reciclada, ecologias y biodegradables. Ideales para utilizar tanto en el interior com en el exterior</p>
+<img src="assets/img/productos/productos/maceta yerba.webp" class="col-12 d-flex p-3 d-lg-none align-self-center  img-modal">
+<div class="col-11 d-flex d-lg-none justify-content-center">
+<p class="col-12 col-sm-6">* Venta al por mayor a partir de 20 piezas</p>
+</div>
+<div class="btn-category pb-2 p-0 col-11 d-flex flex-wrap justify-content-evenly">
+    <button class="btn btn-light col-5 m-1">Chico</button>
+    <button class="btn btn-light col-5 m-1">Mediano</button>
+    <button class="btn btn-light col-5 m-1">Grande</button>
+    <button class="btn btn-light col-5 m-1">Extragrande</button>
+</div>
+<div class="text-center col-11"> 
+    <div class="may-men p-0 p-lg-2 d-flex justify-content-evenly">
     <h2 class="f-reg">Menor <b>$320</b></h2>
     <h2 class="f-reg">Mayor <b>$230</b></h2>
     </div>
-    <div class="mas-men">
+    <div class="p-1 p-lg-2">
     <button class="btn btn-modal">-</button>
     <button class="btn btn-modal-l">1</button>
     <button class="btn btn-modal">+</button>
@@ -30,7 +37,6 @@ modal1.innerHTML +=
     <button class="btn btn-quiero2"> Quiero Comprar</button>
 </div>
 </div>
-<a href="#" class="modal__close">Cerrar Modal</a>
 </div>
 </section>
 
