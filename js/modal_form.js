7ee9269fc29be16b04d51ebb3985425f9ee1d4e7
Ayrton Diaz <89177834+ayrtondiaz@users.noreply.github.com>
2023-01-23 18:45:25 -0300

    Update modal_form.js

--- js/modal_form.js
+++ js/modal_form.js
@@ -4,7 +4,7 @@ modalContainer.innerHTML=`
 <div class="modal_container-form row text-center">
   <div class="col-10">
   <section class="escribinos">
-    <form action="https://formsubmit.co/diazstudio99@gmail.com" method="POST" enctype="multipart/form" id="form"> 
+    <form action="https://formsubmit.co/xxxx@gmail.com" method="POST" enctype="multipart/form" id="form"> 
       <fieldset>
           <legend class="f-bold" style="color: #1d4731;">Quiero participar</legend>
 
